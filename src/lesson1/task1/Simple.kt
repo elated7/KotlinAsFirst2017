559c2117a6e66179733c611e8066ce5abf4d32ab
Kornilov <kornilovartem30@gmail.com>
2017-09-12 13:39:18 +0300

    Решены все "тривиальные" задачи кроме одной .

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -27,7 +27,7 @@ fun sqRoot(a: Double, b: Double, c: Double) = (-b + sqrt(discriminant(a, b, c)))
 /**
  * Пример
  *
- * Поиск произведения корней квадратного уравнения
+ * Поиск произведения кор   ней квадратного уравнения
  */
 fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
     val sd = sqrt(discriminant(a, b, c))
@@ -51,7 +51,7 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int =hours*3600+minutes*60+seconds
 
 /**
  * Тривиальная
@@ -60,7 +60,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =(sagenes * 48 + arshins * 16 + vershoks)*0.04445
 
 /**
  * Тривиальная
@@ -68,7 +68,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double = PI/180 * (grad+min / 60.0 + sec / 3600.0)
 
 /**
  * Тривиальная
@@ -76,7 +76,7 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =
 
 /**
  * Простая
